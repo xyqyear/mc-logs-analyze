9daac5fda46c548ee482ed5238e6f6a412511119
xyqyear <xyqyear@gmail.com>
2024-12-24 14:23:44 +0800

    feat: update terminology for total messages to chat messages in LaTeX frame

--- overall_frames/total_messages.py
+++ overall_frames/total_messages.py
@@ -1,19 +1,20 @@
 from stats import get_total_messages
 
+
 def write_frame(dfs, frames_dir):
     """Generate the total messages frame"""
     total_msgs = get_total_messages(dfs)
-    
+
     content = [
-        "\\begin{frame}{玩家总发言数}",
+        "\\begin{frame}{玩家总聊天数}",
         "\\begin{center}",
         "\\vspace{2em}",
         "{\\Huge",
         f"{total_msgs['total_messages']:,} 条",  # Use comma as thousand separator
         "}",
         "\\end{center}",
-        "\\end{frame}"
+        "\\end{frame}",
     ]
-    
+
     with open(f"{frames_dir}/total_messages.tex", "w", encoding="utf-8") as f:
         f.write("\n".join(content))
